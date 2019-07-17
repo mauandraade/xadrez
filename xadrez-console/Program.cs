08182961b3a907404a06af98187a631020889333
Mauricio <mau_andraade@hotmail.com>
2019-07-17 14:38:08 -0300

    Tratando erros com exceçao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,16 +8,20 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            try { 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(tab, Cor.Preta),new Position(0, 0));
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Position(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Position(2, 4));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Position(0, 9));
            
 
             Tela.imprmirTabuleiro(tab);
-
-
+            }
+            catch(TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            
+            }
             Console.ReadLine();
         }
     }
--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -21,10 +21,41 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Position pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Position pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p, Position pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Ja existe uma peça nessa posição ");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+
+        public bool posicaoValida(Position pos)
+        {
+            if(pos.linha <0 || pos.linha >=linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Position pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao invalida");
+            }
         }
     }
 }
